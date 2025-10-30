c6c8b4c1cfca73aff654600fc11526c4c6299c84
ComfortGroupTelegaBot <adildisel@mail.ru>
2025-10-31 00:41:35 +0300

    add config for telegram bot

---
+++ comfortgrouptelegabot/src/main/java/com/example/comfortgrouptelegabot/config/BotInit.java
@@ -0,0 +1,21 @@
+package com.example.comfortgrouptelegabot.config;
+
+import com.example.comfortgrouptelegabot.telegram.TelegramBot;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class BotInit {
+
+    @SneakyThrows
+    @Bean
+    public TelegramBotsApi telegramBotsApi(TelegramBot telegramBot) {
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(telegramBot);
+        return api;
+    }
+}
